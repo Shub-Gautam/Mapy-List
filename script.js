1b11c26401934715cdf27fbb1e8111c0e8634252
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-07-25 23:52:49 +0530

    adding func

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 let cords = [];
 
-const get_cords = function () {
+const get_cords = function (cords) {
   const data = navigator.geolocation.getCurrentPosition((position) => {
     const { longitude } = position.coords;
     const { latitude } = position.coords;
     // console.log(latitude, longitude);
-    cords = [latitude, longitude];
+    this.cords = [latitude, longitude];
     // console.log(cords);
   });
 };
+class App {
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling clicks on map
+    this.#map.on("click", this._showForm.bind(this));
+
+    this.#workouts.forEach((work) => {
+      this._renderWorkoutMarker(work);
+    });
+  }
+}
 
 get_cords();
+
+
 console.log(cords);
 
 var map = L.map("map").setView([51.505, -0.09], 13);
